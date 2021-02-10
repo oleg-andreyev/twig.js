245568470a2c7f1bb35bb931dbe7e93eb40bb77c
Andrejs Kovtuns <andkovt@gmail.com>
2021-02-10 22:05:22 +0200

    Fixing PR comments

--- src/TwigJs/Compiler/ForCompiler.php
+++ src/TwigJs/Compiler/ForCompiler.php
@@ -91,7 +91,7 @@ class ForCompiler implements TypeCompilerInterface
 
         $compiler
             ->write(sprintf('var %s = %s;', $loopName, json_encode($loopData, JSON_THROW_ON_ERROR)))
-            ->write('\n');
+            ->write("\n");
 
         if (false === $node->getAttribute('ifexpr')) {
             $compiler
